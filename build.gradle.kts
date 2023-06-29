aefd991be5cb8dc04d2b7abcd5e4d810cf5a62aa
rarspace01 <rarspace01@github.com>
2023-06-29 11:05:50 +0200

    publish to GH

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,31 +47,14 @@ tasks.test {
 publishing {
     publications.create<MavenPublication>("maven") {
         from(components["java"])
-        pom {
-            name.set("My Library")
-            description.set("A concise description of my library")
-            url.set("http://www.example.com/library")
-            properties.set(mapOf(
-                "myProp" to "value",
-                "prop.with.dots" to "anotherValue"
-            ))
-            licenses {
-                license {
-                    name.set("The Apache License, Version 2.0")
-                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/rarspace01/mrz-java")
+                credentials {
+                    username = "rarspace01"
+                    password = System.getenv("AUTOPR")
                 }
-            }
-            developers {
-                developer {
-                    id.set("johnd")
-                    name.set("John Doe")
-                    email.set("john.doe@example.com")
-                }
-            }
-            scm {
-                connection.set("scm:git:git://example.com/my-library.git")
-                developerConnection.set("scm:git:ssh://example.com/my-library.git")
-                url.set("http://example.com/my-library/")
             }
         }
     }
