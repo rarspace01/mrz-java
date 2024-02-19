5fe2ccf6ce521c8f2afdd8ea0ff60c0af7026450
rarspace01 <rarspace01@github.com>
2024-02-19 13:44:50 +0100

    update java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,11 +28,11 @@ dependencies {
 group = "rarspace01"
 version = "0.9-SNAPSHOT"
 description = "MRZ Java Parser"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
     withJavadocJar()
 }
 
